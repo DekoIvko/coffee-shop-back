7188e57b30adc971865959c78ebfc78773cef013
DekoIvko <ivkovski89@hotmail.com>
2023-11-11 14:14:05 +0100

    remove console logs

--- routes/deliverRoutes.js
+++ routes/deliverRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const EventEmitter = require("node:events");
 
 // controllers
 const get_delivers = require("../controllers/get_delivers");
 const add_delivers = require("../controllers/add_delivers");
-// const remove_all_myCoffees = require("../controllers/remove_all_myCoffees");
 
 module.exports = function () {
-  const emitter = new EventEmitter();
   const app = express.Router();
   app.use(bodyParser.json()); //  application/json
   app.use(bodyParser.urlencoded({ extended: true })); // application/x-www-form-urlencoded
@@ -18,13 +15,7 @@ module.exports = function () {
   });
 
   app.post("/add-deliver", (req, res) => {
-    // console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    // await emitter.on("remove-myOrders", (myCoffees) => {
-    // remove_all_myCoffees.ServiceCall(myCoffees);
-    // console.log(myCoffees);
-    // });
     add_delivers.ServiceCall(req, res);
-    // console.log("bbbbbbbbbbbbb");
   });
 
   return app;
